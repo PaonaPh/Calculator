60f09abc2fd621d99883aa6e7c12614dbce74de0
PaonaPh <paonasharma@gmail.com>
2021-08-29 21:52:08 +0530

    removed all warnings

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+//import android.util.Log;
+//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.calculator.R;
+//import com.example.calculator.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText1, editText2;
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String first, second;
     private  int f,s;
-    private double total;
+    private int total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,69 +32,58 @@ public class MainActivity extends AppCompatActivity {
         divbtn = findViewById(R.id.divBtn);
         modbtn = findViewById(R.id.modBtn);
 
-        addbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                first = editText1.getText().toString();
-                second = editText2.getText().toString();
-                f = Integer.parseInt(first);
-                s = Integer.parseInt(second);
-                total = f+s;
-                result.setText(String.valueOf(total));
-            }
-        });
+        addbtn.setOnClickListener(v -> add());
+        subbtn.setOnClickListener(v -> sub());
 
-        subbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                first = editText1.getText().toString();
-                second = editText2.getText().toString();
-                f = Integer.parseInt(first);
-                s = Integer.parseInt(second);
-                total = f-s;
-                result.setText(String.valueOf(total));
-            }
-        });
+        mulbtn.setOnClickListener(v -> mul());
 
-        mulbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                first = editText1.getText().toString();
-                second = editText2.getText().toString();
-                f = Integer.parseInt(first);
-                s = Integer.parseInt(second);
-                total = f*s;
-                result.setText(String.valueOf(total));
-            }
-        });
+        divbtn.setOnClickListener(v -> div());
 
-        divbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                first = editText1.getText().toString();
-                second = editText2.getText().toString();
-                f = Integer.parseInt(first);
-                s = Integer.parseInt(second);
-                double divide = ((double)f/s);
-                result.setText(String.valueOf(divide));
-            }
-        });
+        modbtn.setOnClickListener(v -> mod());
+    }
 
-        modbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                first = editText1.getText().toString();
-                second = editText2.getText().toString();
-                f = Integer.parseInt(first);
-                s = Integer.parseInt(second);
-                total = f%s;
-                result.setText(String.valueOf(total));
-            }
-        });
+    private void add(){
+        first = editText1.getText().toString();
+        second = editText2.getText().toString();
+        f = Integer.parseInt(first);
+        s = Integer.parseInt(second);
+        total = f+s;
+        result.setText(String.valueOf(total));
+    }
+
+    private void sub(){
+        first = editText1.getText().toString();
+        second = editText2.getText().toString();
+        f = Integer.parseInt(first);
+        s = Integer.parseInt(second);
+        total = f-s;
+        result.setText(String.valueOf(total));
+    }
+
+    private void mul(){
+        first = editText1.getText().toString();
+        second = editText2.getText().toString();
+        f = Integer.parseInt(first);
+        s = Integer.parseInt(second);
+        total = f*s;
+        result.setText(String.valueOf(total));
+    }
+
+    private void div(){
+        first = editText1.getText().toString();
+        second = editText2.getText().toString();
+        f = Integer.parseInt(first);
+        s = Integer.parseInt(second);
+        double divide = ((double)f/s);
+        result.setText(String.valueOf(divide));
+    }
+
+    private void mod(){
+        first = editText1.getText().toString();
+        second = editText2.getText().toString();
+        f = Integer.parseInt(first);
+        s = Integer.parseInt(second);
+        total = f%s;
+        result.setText(String.valueOf(total));
     }
 }
